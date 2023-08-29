42478f23d894d7dc54333f70e39c3fa51f59fa0d
alnsuarez <alansuarezsan@gmail.com>
2023-08-29 10:41:46 -0600

    Added match functionality, filter for dogs and login site

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import DogSite from './components/dogsSite/DogSite';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import Login from './components/auth/Login';
+import { NextUIProvider } from '@nextui-org/react';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Login />,
+    },
+    {
+        path: '/DogSite',
+        element: (
+            <ProtectedRoute>
+                <DogSite />
+            </ProtectedRoute>
+        ),
+    },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <NextUIProvider>
+            <RouterProvider router={router} />
+        </NextUIProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
